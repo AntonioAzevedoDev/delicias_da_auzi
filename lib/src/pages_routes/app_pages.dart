a5796e5fca8dd2b6e73b772a01db7728d2989595
Antonio Azevedo <antonio.azevedo@evtit.com>
2022-07-29 22:10:57 -0300

    Create Page Routes and AuthController

---
+++ lib/src/pages_routes/app_pages.dart
@@ -0,0 +1,33 @@
+import 'package:delicias_da_auzi/src/pages/auth/sign_in_screen.dart';
+import 'package:delicias_da_auzi/src/pages/auth/sign_up_screen.dart';
+import 'package:delicias_da_auzi/src/pages/base/base_screen.dart';
+import 'package:delicias_da_auzi/src/pages/splash/splash_screen.dart';
+import 'package:get/get.dart';
+
+abstract class AppPages{
+  static final pages = <GetPage>[
+    GetPage(
+        page: () => const SplashScreen(),
+        name: PagesRoutes.splashRoute
+    ),
+    GetPage(
+        page: () => SignInScreen(),
+        name: PagesRoutes.signInRoute
+    ),
+    GetPage(
+        page: () => SignUpScreen(),
+        name: PagesRoutes.signUpRoute
+    ),
+    GetPage(
+        page: () => BaseScreen(),
+        name: PagesRoutes.baseRoute
+    ),
+  ];
+}
+
+abstract class PagesRoutes{
+  static const String signInRoute = '/signin';
+  static const String signUpRoute = '/signup';
+  static const String splashRoute = '/splash';
+  static const String baseRoute = '/base';
+}
